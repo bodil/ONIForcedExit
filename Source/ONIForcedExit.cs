1662c092d8bf106e2d8ff54ca993ee9077951f5b
Bodil Stokke <bodil@bodil.org>
2021-05-26 17:13:05 +0100

    An ingame clock with informative tooltips.

--- Source/ONIForcedExit.cs
+++ Source/ONIForcedExit.cs
@@ -33,13 +33,41 @@ namespace ONIForcedExit
         [JsonProperty]
         public int ExitAt { get; set; }
 
+        public enum ClockFormatType
+        {
+            [Option("System locale", "Use the clock format defined by your operating system's locale.")]
+            FollowLocale,
+            [Option("24 hour", "Use a 24 hour clock.")]
+            TwentyFourHour,
+            [Option("12 hour", "Use a 12 hour clock.")]
+            TwelveHour,
+        }
+
+        [Option("Clock format")]
+        [JsonProperty]
+        public ClockFormatType ClockFormat { get; set; }
+
+        [Option("In-game clock", "Display the current time in the top left status area.")]
+        [JsonProperty]
+        public bool IngameClock { get; set; }
+
         public Options()
         {
             ExitAfterMode = true;
             ExitAfter = 12;
             ExitAtMode = false;
             ExitAt = 0;
+            ClockFormat = ClockFormatType.FollowLocale;
+            IngameClock = false;
         }
+
+        public string RenderTime(System.DateTime time) => ClockFormat switch
+        {
+            ClockFormatType.FollowLocale => time.ToShortTimeString(),
+            ClockFormatType.TwentyFourHour => time.ToString("H:mm"),
+            ClockFormatType.TwelveHour => time.ToString("h:mm tt"),
+            _ => throw new System.ArgumentException(nameof(ClockFormat), "invalid enum value for ClockFormat")
+        };
     }
 
     public class ForcedExit
@@ -106,10 +134,83 @@ namespace ONIForcedExit
             {
                 if (System.DateTime.Now.Hour == Options.Instance.ExitAt)
                 {
-                    Game.Instance.StartDelayed(10, () => ExitNow($"It is now {System.DateTime.Now.ToShortTimeString()}."));
+                    var currentTime = Options.Instance.RenderTime(System.DateTime.Now);
+                    Game.Instance.StartDelayed(10, () => ExitNow($"It is now {currentTime}."));
                     return;
                 }
             }
+        }
+    }
+
+    public class Clock : PTextComponent
+    {
+        public static Clock? Instance = null;
+
+        public static void Instantiate(MeterScreen parentScreen)
+        {
+            Instance = new Clock();
+            PUIUtils.AddTo(Instance, parentScreen.gameObject);
+        }
+
+        public static void DestroyInstance() { Instance = null; }
+
+        public Clock() : base("ClockDisplay")
+        {
+            TextAlignment = TextAnchor.MiddleLeft;
+        }
+
+        private LocText? text;
+        public ToolTip? tooltip;
+
+        public override GameObject Build()
+        {
+            var label = PUIElements.CreateUI(null, base.Name);
+            text = PTextComponent.TextChildHelper(label, PUITuning.Fonts.UILightStyle, Options.Instance.RenderTime(System.DateTime.Now));
+            tooltip = EntityTemplateExtensions.AddOrGet<ToolTip>(label);
+            tooltip.OnToolTip = OnToolTip;
+            label.SetActive(true);
+            var layout = label.AddComponent<RelativeLayoutGroup>();
+            layout.Margin = new RectOffset(10, 10, 8, 8);
+            ArrangeComponent(layout, text.gameObject, base.TextAlignment);
+            if (!DynamicSize) layout.LockLayout();
+            layout.flexibleWidth = FlexSize.x;
+            layout.flexibleHeight = FlexSize.y;
+            DestroyLayoutIfPossible(label);
+            InvokeRealize(label);
+            return label;
+        }
+
+        public void Update()
+        {
+            if (text is not null)
+            {
+                text.text = Options.Instance.RenderTime(System.DateTime.Now);
+            }
+        }
+
+        public string OnToolTip()
+        {
+            if (tooltip is not null)
+            {
+                tooltip.ClearMultiStringTooltip();
+
+                var timePlayed = System.DateTime.Now - ForcedExit.GameStarted;
+                if (Options.Instance.ExitAfterMode)
+                {
+                    var exitTime = ForcedExit.GameStarted.AddHours(Options.Instance.ExitAfter);
+                    tooltip.AddMultiStringTooltip($"You have been playing for <color=#F0B310FF>{timePlayed.Hours}</color> of <color=#F0B310FF>{Options.Instance.ExitAfter}</color> hours.", PUITuning.Fonts.TextLightStyle);
+                }
+                else
+                {
+                    tooltip.AddMultiStringTooltip($"You have been playing for <color=#F0B310FF>{timePlayed.Hours}</color> hours.", PUITuning.Fonts.TextLightStyle);
+                }
+                if (Options.Instance.ExitAtMode)
+                {
+                    var exitTime = Options.Instance.RenderTime(System.DateTime.Today.AddHours(Options.Instance.ExitAt));
+                    tooltip.AddMultiStringTooltip($"Your game will exit at <color=#F0B310FF>{exitTime}</color>.", PUITuning.Fonts.TextLightStyle);
+                }
+            }
+            return "";
         }
     }
 
@@ -121,6 +222,15 @@ namespace ONIForcedExit
             {
                 PUtil.InitLibrary();
                 POptions.RegisterOptions(typeof(Options));
+
+                foreach (var img in Resources.FindObjectsOfTypeAll<Sprite>())
+                {
+                    var name = img?.name?.ToLower();
+                    if (name is not null)
+                    {
+                        if (name.Contains("schedule") || name.Contains("time") || name.Contains("clock")) PUtil.LogDebug($"Sprite: {name}");
+                    }
+                }
             }
         }
 
@@ -128,6 +238,42 @@ namespace ONIForcedExit
         public static class Game_Save_Patch
         {
             public static void Postfix() => ForcedExit.CheckForcedExit();
+        }
+
+        [HarmonyPatch(typeof(MeterScreen), "OnPrefabInit")]
+        public static class MeterScreen_OnPrefabInit_Patch
+        {
+            public static void Postfix(MeterScreen __instance)
+            {
+                if (Options.Instance.IngameClock)
+                {
+                    Clock.Instantiate(__instance);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(MeterScreen), "DestroyInstance")]
+        public static class MeterScreen_DestroyInstance_Patch
+        {
+            public static void Postfix()
+            {
+                if (Options.Instance.IngameClock)
+                {
+                    Clock.DestroyInstance();
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(MeterScreen), "Refresh")]
+        public static class MeterScreen_Refresh_Patch
+        {
+            public static void Postfix()
+            {
+                if (Options.Instance.IngameClock)
+                {
+                    Clock.Instance?.Update();
+                }
+            }
         }
     }
 }
